213807c99d817a69eb0f3c99d85c878b82f4cc49
Ksenia <ershova.kseniya2911@gmail.com>
2021-11-26 10:57:39 +0500

    24.11.21

---
+++ 19.11.21classwork/php_primer.php
@@ -0,0 +1,4 @@
+<?php
+$str1 = "hello";
+$str2 = "world";
+
--- 22.11.21mypage/html/index.php
+++ 22.11.21mypage/html/index.php
@@ -1,4 +1,25 @@
-<!DOCTYPE html>
+<?php
+if(date("H") > 20.01 && date("H") < 07.59){
+    $style = 'style-night.css';
+}else{
+    $style = 'style.css';
+}
+?>
+<!Doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel='stylesheet' href='<php echo $style; ?/>' type='text/css' media='all'>
+
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html><html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
