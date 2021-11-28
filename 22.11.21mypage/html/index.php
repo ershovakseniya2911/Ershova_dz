02160da48078359d0c9edc765202c7dbbd1be03f
Ksenia <ershova.kseniya2911@gmail.com>
2021-11-29 00:49:16 +0500

    26.11.21

--- 22.11.21mypage/html/index.php
+++ 22.11.21mypage/html/index.php
@@ -1,7 +1,7 @@
 <?php
-if(date("H") > 20.01 && date("H") < 07.59){
+if((date("H")) > 20.01 && date("H") < 07.59){
     $style = 'style-night.css';
-}else{
+} else{
     $style = 'style.css';
 }
 ?>
@@ -12,9 +12,9 @@ if(date("H") > 20.01 && date("H") < 07.59){
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel='stylesheet' href='<php echo $style; ?/>' type='text/css' media='all'>
+    <link rel='stylesheet' type="text/css" href='<?php echo $style; ?>'>
 
-    <title>Document</title>
+    <title>Mysite</title>
 </head>
 <body>
 
